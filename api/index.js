b194fad6605e7dd67d73cec0d4f888f852f44b2a
RishiChaubey31 <cenationrishi31@gmail.com>
2024-12-28 02:01:36 +0530

    .

--- api/index.js
+++ api/index.js
@@ -221,6 +221,6 @@ app.get("/scrape", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 module.exports = app;
